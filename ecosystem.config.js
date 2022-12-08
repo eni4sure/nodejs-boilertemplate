d097893b89743f1922aef5a01b9d7627bc19549e
Eniola Osabiya <eosabiya@gmail.com>
2022-12-08 19:16:53 +0100

    chore: remove pm2 deploy script

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -8,18 +8,5 @@ module.exports = {
             watch: ".",
             ignore_watch: ["node_modules"]
         }
-    ],
-
-    deploy: {
-        production: {
-            user: "SSH_USERNAME",
-            host: "SSH_HOSTMACHINE",
-            ref: "origin/main",
-            repo: "GIT_REPOSITORY",
-            path: "DESTINATION_PATH",
-            "pre-deploy-local": "",
-            "post-deploy": "npm install && pm2 reload ecosystem.config.js --env production",
-            "pre-setup": ""
-        }
-    }
+    ]
 };
