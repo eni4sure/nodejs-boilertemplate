797feb8d0efd69b515466e5fcd345752dcb82949
Eniola Osabiya <eosabiya@gmail.com>
2023-06-12 15:26:05 -0500

    chore: update some config parsing

--- src/configs/index.ts
+++ src/configs/index.ts
@@ -12,7 +12,7 @@ const CONFIGS = {
 
     MONGODB_URI: process.env.MONGO_ATLAS_URI || "mongodb://127.0.0.1:27017/nodejs-boilertemplate",
 
-    BCRYPT_SALT: process.env.BCRYPT_SALT || 10,
+    BCRYPT_SALT: Number(process.env.BCRYPT_SALT) || 10,
 
     JWT_SECRET: process.env.JWT_SECRET || "000-12345-000",
     JWT_EXPIRES_IN: process.env.JWT_EXPIRES_IN || "1h",
@@ -33,7 +33,7 @@ const CONFIGS = {
         SMTP_PORT: process.env.MAILER_SMTP_PORT,
         SMTP_USER: process.env.MAILER_SMTP_USER,
         SMTP_PASSWORD: process.env.MAILER_SMTP_PASSWORD,
-        SECURE: process.env.MAILER_SECURE || false,
+        SECURE: process.env.MAILER_SECURE === "true" ? true : false,
     },
 
     SENTRY: {
